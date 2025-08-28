f12c47a160ad9f25526fff67520794128abb5c58
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-08-28 16:31:22 -0500

    initial testing
    testing is done to make sure data is in the database in a correct form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing  = require("./models/listing.js")
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/myairbnb";
@@ -17,7 +18,20 @@ async function main(){
 
 app.get("/",(req,res) =>{
     res.send("Ready to serve");
-})
+});
+
+app.get("/testListing",async(req,res) =>{
+    let sampleListing = new Listing({
+        title:"Dallas Home",
+        description:"Avaialbe for 2 days.It is closeby downtown.",
+        price:350,
+        location:"Dallas,TX",
+        country:"United States",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("succesfull testing");
+});
 
 app.listen(8080,() =>{
     console.log("server is listening to port 8080");
