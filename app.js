43426528858dce6f5b14efff47e11893769f9632
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-08-27 20:26:55 -0500

    server setup and mongoDB setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/myairbnb";
+
+main().then(() =>{
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req,res) =>{
+    res.send("Ready to serve");
+})
+
+app.listen(8080,() =>{
+    console.log("server is listening to port 8080");
+})
