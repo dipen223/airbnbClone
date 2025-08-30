72926ab10d2c9976d220f09eaa8321e0e82e540b
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-08-29 20:43:11 -0500

    add a new show route that shows the details about the listings via id

--- app.js
+++ app.js
@@ -19,19 +19,26 @@ async function main(){
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res) =>{
     res.send("Ready to serve");
 });
 
 
+// Index Route
 app.get("/listings",async (req,res) =>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
 });
 
 
-
+// show Route
+app.get("/listings/:id",async(req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+})
 
 app.listen(8080,() =>{
     console.log("server is listening to port 8080");
