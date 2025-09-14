bf6963511207f4e84478e2b54413760728b6bafa
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-14 18:47:44 -0500

    add a error flash message to all the related routes

--- app.js
+++ app.js
@@ -48,8 +48,11 @@ app.use(flash())
 
 app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+
     next();
 })
+
 
 
 app.use("/listings",listings);
