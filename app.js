d03eb2f706116ea5822ab05c9a730877c4d50ef4
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-13 22:21:42 -0500

    move review routes to the routes folder for better structure

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
-const { listingSchema,reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/myairbnb";
 main().then(() => {
@@ -22,7 +19,6 @@ main().then(() => {
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -34,20 +30,8 @@ app.get("/", (req, res) => {
     res.send("Ready to serve");
 });
 
-
-const validateReview = (req, res, next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el) =>el.message).join(",");
-        throw new expressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-
-}
-
 app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 //I am getting an errro whenever i am trying to use this.
 // app.all("*",(req,res,next) =>{
@@ -56,26 +40,6 @@ app.use("/listings",listings);
 
 // Reviews 
 //  ~post route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res) =>{
-    let listing  = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-
-    console.log("new review saved");
-    res.redirect(`/listings/${listing._id}`)
-
-}));
-
-// delete review route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res) => {
-    let {id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findById(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
-
 
 app.use((req, res, next) => {
     res.status(404).send("Page Not Found");
