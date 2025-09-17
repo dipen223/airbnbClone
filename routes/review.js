a291ca006717c92642bc9fd36c900542ac595e11
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-17 07:37:17 -0500

    Move validateReview middleware to middleware file

--- routes/review.js
+++ routes/review.js
@@ -2,20 +2,10 @@ const express = require("express");
 const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const expressError = require("../utils/expressError.js");
-const {reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
+const {validateReview} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el) =>el.message).join(",");
-        throw new expressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
 
 // post review
 router.post("/",validateReview,wrapAsync(async(req,res) =>{
