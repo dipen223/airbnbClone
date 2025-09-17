48e0f33604712747d154f95dec0265df2454d363
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-16 22:35:42 -0500

    ..

--- routes/listing.js
+++ routes/listing.js
@@ -46,6 +46,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/",isLoggedIn,validateListing, wrapAsync(async (req, res, next) => {
     
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
