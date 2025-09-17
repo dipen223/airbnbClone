655adf2397967a1146188c0eb1b01ca437d430e5
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-16 22:15:28 -0500

    add a new owner property in the listing object
    to associate each listing with owner

--- init/index.js
+++ init/index.js
@@ -17,6 +17,7 @@ async function main(){
 
 const initDB = async() =>{
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) =>({...obj,owner:'68c9dd128874644ca2bb134f'}));
     await Listing.insertMany(initData.data);
     console.log("data was initialzed");
 }
