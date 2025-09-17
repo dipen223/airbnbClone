f263ccf3da9697fe308ec73560124d9cb0d1efb5
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-17 07:36:37 -0500

    create an isOwner middleware to check if the user is authorized to make changes in listing
    move all the middlewares to this file

--- middleware.js
+++ middleware.js
@@ -1,3 +1,6 @@
+const Listing = require("./models/listing");
+const expressError = require("./utils/expressError.js");
+const { listingSchema,reviewSchema} = require("./schema.js");
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         // redirect url
@@ -14,3 +17,36 @@ module.exports.saveRedirectUrl = (req,res,next) =>{
     }
     next();
 };
+
+module.exports.isOwner = async(req,res,next) =>{
+     let { id } = req.params;
+    let listing = await Listing.findById(id);
+        if(!listing.owner.equals(res.locals.currUser._id)){
+            req.flash("error","You're not the owner of this listing");
+            return res.redirect(`/listings/${id}`);
+        }
+        next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) =>el.message).join(",");
+        throw new expressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+
+}
+
+module.exports.validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) =>el.message).join(",");
+        throw new expressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+}
