8b6e2407afe12745a9e53b198cda81c6879f2322
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-16 22:18:47 -0500

    chain a populate method to populate owner details

--- routes/listing.js
+++ routes/listing.js
@@ -33,7 +33,7 @@ router.get("/new",isLoggedIn,(req, res) => {
 // show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error","Listing you requested for doesn't exist!");
         return res.redirect("/listings");
