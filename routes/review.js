ad45d124f12f477b3aec001d14580f89898ba5e4
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-17 17:02:55 -0500

    add an author fild in a new Review

--- routes/review.js
+++ routes/review.js
@@ -4,17 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const expressError = require("../utils/expressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview,isLoggedIn} = require("../middleware.js");
 
 
 // post review
-router.post("/",validateReview,wrapAsync(async(req,res) =>{
+router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res) =>{
     let listing  = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author =  req.user._id;
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
-
    req.flash("success","New Review Added!");
     res.redirect(`/listings/${listing._id}`)
 
