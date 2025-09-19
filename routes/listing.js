a5330656b410de1ac585262fd035940c5b5e5744
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-19 17:07:54 -0500

    compact similar routes in a router.route method
    avoids router naming duplication

--- routes/listing.js
+++ routes/listing.js
@@ -5,26 +5,24 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing}  = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
-// Index Route
-router.get("/", wrapAsync(listingController.index));
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(isLoggedIn,validateListing, wrapAsync(listingController.createListing)
+);
 
 // new route 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
-// show Route
-router.get("/:id", wrapAsync(listingController.showListing));
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
 
-// Create Route
-router.post("/",isLoggedIn,validateListing, wrapAsync(listingController.createListing)
-);
 
 // Edit route
 router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
 
-// update route
-router.put("/:id", isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
-
-// delete route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,16 +5,15 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const session = require("express-session");
-
 const userController = require("../controllers/users.js");
 
-router.get("/signup",userController.renderSignUpForm);
+router.route("/signup")
+.get(userController.renderSignUpForm)
+.post(wrapAsync(userController.signup));
 
-router.post("/signup",wrapAsync(userController.signup));
-
-router.get("/login",userController.renderLoginForm);
-
-router.post("/login",saveRedirectUrl,
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,
     passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),userController.login);
 
 
