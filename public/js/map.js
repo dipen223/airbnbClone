f780e56bd236318feff3f30b69cdf7e8aa4727b3
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-21 20:26:17 -0500

    add a map marker using Marker feature of mapbox

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,10 @@
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [-96.7970,32.7767], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center:coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+const marker = new mapboxgl.Marker({color:"red"})
+        .setLngLat(coordinates)
+        .addTo(map);
