18d14eaa6e4c47d57f9dfe46b5c62db9a94f8540
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-09-22 21:03:30 -0500

    add a map popup in the marker

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,12 @@
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center:coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center:listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
-const marker = new mapboxgl.Marker({color:"red"})
-        .setLngLat(coordinates)
-        .addTo(map);
+const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${listing.title}</h4><p>Exact location will be  provided after booking</p>`);
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(popup)
+  .addTo(map);
